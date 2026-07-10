31a81725269b0a2144f3db50947bcef7816bc697
chidozie-daniel <dchidozie06@gmail.com>
2026-07-10 18:59:44 +0530

    just practicing flexbox

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,8 @@
 console.log("Hello World");
 console.log("I'm Daniel...");
 console.log("I am Learning JavaScript");
+console.log(2026);
+console.log("I want to become a MERN Stack developer");
+console.log("JavaScript is my main language");
+console.log(10 + 2);
+console.log("Experimenting is one of the fastest ways to build intuition.");
